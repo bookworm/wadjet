a7d75226c27df2d777c5129eeff4de3be2c57728
Ken Erickson <bookworm.productions@gmail.com>
2011-09-22 13:09:53 -0700

    REST up the fake widget

--- app/controllers/widgetfake.rb
+++ app/controllers/widgetfake.rb
@@ -1,6 +1,6 @@
 Wadjet.controllers :widget_fake do  
   
-  get :render_fragment, :map => '/widgets/fake/render_fragment/:slug'  do
+  get :render, :map => '/widgets/fake/:slug/render'  do
     @widget = current_account.dashboard.widgets(:name => :fake)   
     @widget = @widget.first
     html = render "widgets/#{@widget.view}"      
